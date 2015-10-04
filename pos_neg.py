768d7ee194f8b27dcdf566d0c7936598b2e6e5fd
harishannavajjala <harishannavajjala@gmail.com>
2015-10-04 11:42:28 -0400

    pos_neg.py is used to replace all the underscores to spaces

---
+++ pos_neg.py
@@ -0,0 +1,13 @@
+#To replace all the underscores with spaces 
+f1 = open('/Users/harishannavajjala/Desktop/pos.txt','r+') 
+f2 = open('/Users/harishannavajjala/Desktop/neg.txt','r+')
+old1 = f1.read() # read everything in the file    
+old1 = old1.replace("_", " ")
+old2 = f2.read() # read everything in the file    
+old2 = old2.replace("_", " ")
+
+f1.seek(0) 
+f1.write(old1) 
+f2.seek(0) 
+f2.write(old2) 
+
